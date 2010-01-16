e4d950783cdabc606cc4b699d9544e3ea5e7dd2e
David Sheldon <dave-github@earth.li>
2010-01-16 22:40:08 +0000

    Stephen's improvement to the run script.

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # Optionally pass in the name of a config file on the command line.
-$JAVA_HOME/bin/java -classpath build/src/:lib/*:lib/seleniumdeps/* com.bitclean.billscrape.BillDownload $1 
+if [ -e "$JAVA_HOME/bin/java" ] ; then
+  JAVA="$JAVA_HOME/bin/java"
+else
+  JAVA=java
+fi
+$JAVA -classpath build/src/:lib/*:lib/seleniumdeps/* com.bitclean.billscrape.BillDownload $1 
